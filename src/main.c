93dec0ae8da20bf424a7a8a8be7ed36f47ed9355
pjexposito <pjexposito@gmail.com>
2015-01-04 00:48:27 +0100

    Código comentado. Funciones separadas

--- src/main.c
+++ src/main.c
@@ -4,14 +4,22 @@
 #define COLOR_PRINCIPAL GColorWhite  // El color del lápiz es blanco
 #define COLOR_FONDO GColorBlack  // y el fondo, negro
 
-#define MESES_TURNOS 3
-
+  
+#define MESES_TURNOS 4
+  
+// Matriz de turnos. Se compone de dos dimensiones. La primera marca los meses incluidos en la matrix.
+// Si hay 4 meses de turnos, MESES_TURNOS será 4. Cada turno se marca entre corchetes, como elemento de 
+// la matriz. 
+// La segunda dimensión son los días (o elementos de la matriz MESES_TURNOS). Se compone de 33 elementos.
+// Estos 33 elementos son la suma de los días máximos del mes (31) junto con dos valores iniciales correspondientes
+// al año y al mes al que pertenece la matriz de turnos. Estos valores se usarán más adelante para saber si el
+// mes seleccionado consta de matriz de turnos o no.
 static const char *turnos[MESES_TURNOS][33] = {
 	{ "2015", "1",
     "L", "T", "AT", "FT", "AM", "L", "AM", 
     "M", "L", "L", "FM", "M", "M", "T", 
-    "T", "T", "AT", "D", "AM", "M", "M",
-    "M", "M", "M", "D", "T", "T", "T", 
+    "T", "T", "AT", "L", "AM", "M", "M",
+    "M", "M", "M", "L", "T", "T", "T", 
     "T", "T", "AT"},
   { "2015", "2",
     "L", "L", "M", "M", "M", "M", "M", 
@@ -25,14 +33,25 @@ static const char *turnos[MESES_TURNOS][33] = {
     "L", "M", "M", "M", "L", "M", "M", 
     "L", "T", "T", "T", "T", "T", "AT", 
     "L", "L", "M"}, 
+  { "2014", "12",
+    "M", "M", "T", "T", "AT", "FT", "L",
+    "L", "M", "M", "M", "M", "AM", "FM",
+    "AT", "T", "T", "AT", "M", "AM", "FT", 
+    "AM", "AM", "AM", "L", "M", "M", "FM", 
+    "FT", "AT", "T"},     
 };
 
+// Matriz básica para transformar el número de mes en el nombre del mes.
 static const char *nombre_mes[13] = 
 	{ "vacio", "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", 
    "noviembre", "diciembre" };
 
+// Se define de forma global las variables día, mes y año (dado que pueden cambiar a lo largo de la ejecución)
+// Mes_actual y dia_actual siempre guardarán el valor del día y el mes en el que se ejecuta el programa (no varían)
+// La variable chkturnos puede tener valor 1 si se muestra el calendario de turnos y valor 0 si se muestra el de días.
 int dia, mes, ano, mes_actual, dia_actual, chkturnos;
 
+// Ventana principal
 static Window *window;
 
 
@@ -40,16 +59,9 @@ static Window *window;
 Layer *CapaLineas; // La capa principal donde se dibuja el calendario
 
 
-
- 
-
-
-
 // Esta función se ejecutará cada vez que se refresque la CapaLineas
 void CapaLineas_update_callback(Layer *me, GContext* ctx) 
 {
-
-
     // Color del fondo y color del trazo
     graphics_context_set_stroke_color(ctx, COLOR_PRINCIPAL);
     graphics_context_set_fill_color(ctx, COLOR_PRINCIPAL);
@@ -58,25 +70,24 @@ void CapaLineas_update_callback(Layer *me, GContext* ctx)
     // 144x168
   
     // Se pintan las líneas de calendario
+    
+    // Lineas horizontales
     graphics_fill_rect(ctx, GRect(2, 30, 140, 2), 0, GCornerNone);  
-
     graphics_fill_rect(ctx, GRect(2, 50, 140, 2), 0, GCornerNone);  
     graphics_fill_rect(ctx, GRect(2, 70, 140, 1), 0, GCornerNone);  
     graphics_fill_rect(ctx, GRect(2, 90, 140, 1), 0, GCornerNone);  
     graphics_fill_rect(ctx, GRect(2, 110, 140, 1), 0, GCornerNone);  
     graphics_fill_rect(ctx, GRect(2, 130, 140, 1), 0, GCornerNone);  
-
     graphics_fill_rect(ctx, GRect(2, 150, 142, 2), 0, GCornerNone);  
-  
+    
+    // Lineas verticales
     graphics_fill_rect(ctx, GRect(2, 30, 1, 120), 0, GCornerNone); 
-
     graphics_fill_rect(ctx, GRect(22, 30, 1, 120), 0, GCornerNone); 
     graphics_fill_rect(ctx, GRect(42, 30, 1, 120), 0, GCornerNone); 
     graphics_fill_rect(ctx, GRect(62, 30, 1, 120), 0, GCornerNone); 
     graphics_fill_rect(ctx, GRect(82, 30, 1, 120), 0, GCornerNone); 
     graphics_fill_rect(ctx, GRect(102, 30, 1, 120), 0, GCornerNone); 
     graphics_fill_rect(ctx, GRect(122, 30, 1, 120), 0, GCornerNone); 
-  
     graphics_fill_rect(ctx, GRect(142, 30, 1, 120), 0, GCornerNone); 
 
     // Se pinta la cabecera con los días de la semana
@@ -96,8 +107,10 @@ void CapaLineas_update_callback(Layer *me, GContext* ctx)
     snprintf(temp, sizeof(temp), "%d", ano);
     graphics_draw_text(ctx, temp, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD), GRect(90, 0, 50, 10), GTextOverflowModeTrailingEllipsis, GTextAlignmentLeft, NULL);
 
-    // Pos es la posición original del primer carácter
+    // Pos es la columna en dónde se escribe el día de la semana.
+    // Se inicia en 2 para centrar el dato en el cuadro del calendario
     int pos = 2;
+    // Linea (fila) en la que se escribe el día de la semana. No tiene valor por que se asigna más adelante
     int linea;
     // La casilla de salida establece dónde está el día 1 de cada mes
     int casilla_salida = dweek(ano,mes,dia)-1;
@@ -112,7 +125,8 @@ void CapaLineas_update_callback(Layer *me, GContext* ctx)
       if (i > 20 && i<28) linea = 111;
       if (i > 27 && i<35) linea = 131;
       if (i > 34) linea = 51;
-      // Si es la última casilla, vuelve al principio
+      
+      // Si es la última columna, vuelve al principio
       if (pos>122) pos = 2;
       
       // El año y el mes son los dos primeros valores de la matrix TURNOS
@@ -159,7 +173,7 @@ void CapaLineas_update_callback(Layer *me, GContext* ctx)
 
 }  // Y termina la función
 
-
+// Se pulsa el botón arriba
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
       mes--;
       if (mes==0) 
@@ -171,6 +185,7 @@ static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   // Se resta un mes al actual y si el mes es inferior a 1, se resta un año
 }
 
+// Se pulsa el botón select
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   if (chkturnos==1)
     chkturnos=0;
@@ -180,6 +195,7 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   // Se usa el select para cambiar entre calendario normal y de turnos
 }
 
+// Se pulsa el botón abajo
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
       mes++;
       if (mes==13) 
@@ -191,6 +207,7 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   // Se suma un mes al actual, y si supera el 12, se suma un año
 }
 
+// Se definen las funciones asociadas a la pulsación de botones
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
@@ -228,7 +245,9 @@ static void init()
   
   // Se establece chkturnos a 1 para mostrar el calendario de turnos en primer lugar. Si es 0, se muestran los días
   chkturnos=1;
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "El primer dia del mes %i, del año %i es %i", mes, ano, dweek(ano,mes,dia));    
+  
+  // Línea de DEBUG, por si acaso. Debe estar desactivada siempre que sea posible
+  //APP_LOG(APP_LOG_LEVEL_DEBUG, "El primer dia del mes %i, del año %i es %i", mes, ano, dweek(ano,mes,dia));    
 
 }
 
@@ -241,10 +260,6 @@ static void deinit()
 
   layer_destroy(CapaLineas);
 
-
-
-
-  
   // Y por último se borra de la memoria la ventana principal
   window_destroy(window);
 }
